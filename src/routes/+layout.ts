116ae1e23e47a131003283718058de79df4a5dc8
rahul.aswin <rahul.aswin@zohocorp.com>
2024-12-17 14:38:40 +0530

    Fix: Added private keys

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public'
+import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/public'
 import type { LayoutLoad } from './$types'
 import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
 
@@ -6,12 +6,12 @@ export const load: LayoutLoad = async ({ fetch, data, depends }) => {
   depends('supabase:auth')
 
   const supabase = isBrowser()
-    ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY, {
+    ? createBrowserClient(SUPABASE_URL, SUPABASE_KEY, {
         global: {
           fetch,
         },
       })
-    : createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY, {
+    : createServerClient(SUPABASE_URL, SUPABASE_KEY, {
         global: {
           fetch,
         },
